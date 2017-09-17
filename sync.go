b215ee9a7b722deaac5a03694560e85da9d73b99
Soulou <leo@unbekandt.eu>
2017-09-17 15:07:31 +0200

    Some refactoring

--- sync.go
+++ sync.go
@@ -36,14 +36,31 @@ func PreserveOwnership(s *FsSyncer) {
 	s.PreserveOwnership = true
 }
 
+type syncInfo struct {
+	path     string
+	fileInfo os.FileInfo
+	stat     *syscall.Stat_t
+}
+
+type syncState struct {
+	timesMap map[string]statTimes
+	inoMap   map[uint64]string
+}
+
 type statTimes struct {
 	atime time.Time
 	mtime time.Time
 }
 
+type unexistingFileRes struct {
+	shouldUpdateTimes bool
+}
+
 func (s *FsSyncer) Sync(src, dst string) error {
-	timesMap := map[string]statTimes{}
-	inoMap := map[uint64]string{}
+	state := syncState{
+		timesMap: map[string]statTimes{},
+		inoMap:   map[uint64]string{},
+	}
 
 	err := filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -52,16 +69,6 @@ func (s *FsSyncer) Sync(src, dst string) error {
 		dstPath := strings.Replace(path, src, dst, 1)
 
 		srcStat, ok := info.Sys().(*syscall.Stat_t)
-		if existingLink, ok := inoMap[srcStat.Ino]; ok {
-			err = os.Link(existingLink, dstPath)
-			if err != nil {
-				return errors.Wrapf(err, "fail to create link from %v to %v", existingLink, dstPath)
-			}
-			return nil
-		} else {
-			inoMap[srcStat.Ino] = dstPath
-		}
-
 		atime := time.Unix(int64(srcStat.Atim.Sec), int64(srcStat.Atim.Nsec))
 		mtime := time.Unix(int64(srcStat.Mtim.Sec), int64(srcStat.Mtim.Nsec))
 		if !ok {
@@ -69,34 +76,23 @@ func (s *FsSyncer) Sync(src, dst string) error {
 		}
 
 		_, err = os.Lstat(dstPath)
-		if err != nil && !os.IsNotExist(err) {
-			return errors.Wrapf(err, "fail to stat %v", dstPath)
-		} else if os.IsNotExist(err) {
-			if info.IsDir() {
-				err := os.MkdirAll(dstPath, info.Mode())
-				if err != nil {
-					return errors.Wrapf(err, "fail to create dst directory %v", dstPath)
-				}
-				timesMap[dstPath] = statTimes{atime: atime, mtime: mtime}
-			} else if info.Mode()&os.ModeSymlink == os.ModeSymlink {
-				linkDst, err := os.Readlink(path)
-				if err != nil {
-					return errors.Wrapf(err, "fail to get link destination of src %v", src)
-				}
-				err = os.Symlink(linkDst, dstPath)
-				if err != nil {
-					return errors.Wrapf(err, "fail to create symlink %v (%v)", dstPath, linkDst)
-				}
-
-			} else {
-				_, err := s.copyFileContent(path, dstPath, info)
-				if err != nil {
-					return errors.Wrapf(err, "fail to copy content from %v to %v", path, dstPath)
-				}
-				timesMap[dstPath] = statTimes{atime: atime, mtime: mtime}
+		if os.IsNotExist(err) {
+			res, err := s.syncUnexistingFile(syncInfo{
+				path:     path,
+				fileInfo: info,
+				stat:     srcStat,
+			}, syncInfo{
+				path: dstPath,
+			}, state)
+			if err != nil {
+				return errors.Wrapf(err, "fail to handle unexisting file %v", path)
 			}
+			if res.shouldUpdateTimes {
+				state.timesMap[dstPath] = statTimes{atime: atime, mtime: mtime}
+			}
+		} else if err != nil {
+			return errors.Wrapf(err, "fail to stat %v", dstPath)
 		}
-
 		return nil
 	})
 
@@ -104,7 +100,7 @@ func (s *FsSyncer) Sync(src, dst string) error {
 		return errors.Wrapf(err, "fail to walk %v", src)
 	}
 
-	for file, times := range timesMap {
+	for file, times := range state.timesMap {
 		err = os.Chtimes(file, times.atime, times.mtime)
 		if err != nil {
 			return errors.Wrapf(err, "fail to set atime and mtime of %v", file)
@@ -112,6 +108,46 @@ func (s *FsSyncer) Sync(src, dst string) error {
 	}
 
 	return nil
+}
+
+func (s *FsSyncer) syncUnexistingFile(src, dst syncInfo, state syncState) (unexistingFileRes, error) {
+	res := unexistingFileRes{}
+
+	if existingLink, ok := state.inoMap[src.stat.Ino]; ok {
+		err := os.Link(existingLink, dst.path)
+		if err != nil {
+			return res, errors.Wrapf(err, "fail to create link from %v to %v", existingLink, dst.path)
+		}
+		return res, nil
+	}
+
+	state.inoMap[src.stat.Ino] = dst.path
+
+	if src.fileInfo.IsDir() {
+		err := os.MkdirAll(dst.path, src.fileInfo.Mode())
+		if err != nil {
+			return res, errors.Wrapf(err, "fail to create dst directory %v", dst.path)
+		}
+		return unexistingFileRes{shouldUpdateTimes: true}, nil
+	}
+
+	if src.fileInfo.Mode()&os.ModeSymlink == os.ModeSymlink {
+		linkDst, err := os.Readlink(src.path)
+		if err != nil {
+			return res, errors.Wrapf(err, "fail to get link destination of src %v", src.path)
+		}
+		err = os.Symlink(linkDst, dst.path)
+		if err != nil {
+			return res, errors.Wrapf(err, "fail to create symlink %v (%v)", dst.path, linkDst)
+		}
+		return res, nil
+	}
+
+	_, err := s.copyFileContent(src.path, dst.path, src.fileInfo)
+	if err != nil {
+		return res, errors.Wrapf(err, "fail to copy content from %v to %v", src.path, dst.path)
+	}
+	return unexistingFileRes{shouldUpdateTimes: true}, nil
 }
 
 func (s *FsSyncer) copyFileContent(src, dst string, info os.FileInfo) (int64, error) {
